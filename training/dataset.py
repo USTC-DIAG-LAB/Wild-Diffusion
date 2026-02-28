5e330346b8e355c3eb2cf3afc0db5fdebc0a8e28
XL-Wang97 <69531199+XL-Wang97@users.noreply.github.com>
2026-02-28 13:52:41 +0800

    Update dataset.py

--- training/dataset.py
+++ training/dataset.py
@@ -1,12 +1,3 @@
-# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# You should have received a copy of the license along with this
-# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
-
-"""Streaming images and labels from datasets created with dataset_tool.py."""
-
 import os
 import numpy as np
 import zipfile
