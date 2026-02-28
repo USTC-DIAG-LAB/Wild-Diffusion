f0f77a3d265d0e1fff7e1467810b6cb6bb17c273
XL-Wang97 <69531199+XL-Wang97@users.noreply.github.com>
2026-02-28 13:51:40 +0800

    Update networks.py

--- training/networks.py
+++ training/networks.py
@@ -1,13 +1,3 @@
-# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# You should have received a copy of the license along with this
-# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
-
-"""Model architectures and preconditioning schemes used in the paper
-"Elucidating the Design Space of Diffusion-Based Generative Models"."""
-
 import numpy as np
 import torch
 from torch_utils import persistence
